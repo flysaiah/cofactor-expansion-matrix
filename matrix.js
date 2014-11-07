9b0f6bf4c179529cd4e5bfe5ef4ffad2547da7ad
flysaiah <mayeis01@yahoo.com>
2014-11-06 20:50:06 -0600

    Created basic Matrix Prototype
    Created basic Matrix prototype that holds an array of arrays that
    contain entries in a column-row format that represents a mathematical
    matrix. Also created a user interface that allows users to input the
    selected values for each entry of the matrix that they would like to
    create

--- matrix.js
+++ matrix.js
@@ -1,1 +1,46 @@
-//Javascript file
+Matrix = function(m, n) {  //Matrix prototype that is created when user clicks 'Create matrix' button
+	this.num_rows = m;
+	this.num_columns = n;
+	this.entries = [];
+	for (var i=0; i<this.num_columns; i++) { //Adds correct number of columns to Matrix
+		this.entries.push([])
+	}
+}
+
+Matrix.prototype = {
+	get_num_rows: function() {
+		return this.num_rows;
+	},
+	get_num_columns: function() {
+		return this.num_columns;
+	},
+	addEntry: function(column, row, entry_value) {
+		this.entries[column][row] = entry_value;
+	}
+}
+
+
+matrixCreate = function() {
+	var theMatrix = new Matrix(5, 5); //Create 5x5 matrix prototype
+	console.log(theMatrix)
+	for (var i=0; i<5; i++) { //Create empty 5x5 matrix
+		for (var j=0; j<5; j++) {
+			var entrybox = document.createElement('INPUT');
+			entrybox.type = "text";
+			entrybox.id = String(i) + String(j);
+			entrybox.value = 0;
+			var matrixdiv = document.getElementById('matrixdiv');
+			matrixdiv.appendChild(entrybox);
+		}
+		var newline = document.createElement('BR')
+		matrixdiv.appendChild(newline);
+	}
+	var submitmatrixbutton = document.createElement('BUTTON'); //Create submit matrix button
+	submitmatrixbutton.innerHTML = "Find Determinant"
+	submitmatrixbutton.onclick = function() {
+		//code here for adding entries
+	}
+	var submitmatrixdiv = document.getElementById('submitmatrixdiv');
+	submitmatrixdiv.appendChild(submitmatrixbutton);
+
+}
